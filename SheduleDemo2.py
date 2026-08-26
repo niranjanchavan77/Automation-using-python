e1f04b33d56f5f8f3986270286999e4993b308a9
Niranjan Jagdish Chavan <chavanniranjan77@gmail.com>
2026-08-26 23:28:14 +0530

    Add files via upload

---
+++ SheduleDemo2.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+import datetime
+
+def Display():
+    print("Jay Ganesh...",datetime.datetime.now())
+
+def main():
+    print("Automation script started ")
+
+    schedule.every(1).minute.do(Display)
+
+    # issue
+
+
+if __name__ == "__main__":
+    main()
