7dff94637c6dbcf685c41468b3270c5018ae61cc
Niranjan Jagdish Chavan <chavanniranjan77@gmail.com>
2026-09-03 23:10:43 +0530

    Add files via upload

---
+++ File_ExistsX.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+
+    if(os.path.exists("Demo.txt")):
+        print("file is present in cureent directory")
+    else:
+        print("there is no such file")
+if __name__ == "__main__":
+    main()
